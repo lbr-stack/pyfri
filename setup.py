67c39d439b27206c68cfa3116b8c7f91f30aaf2c
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-05-06 00:08:00 +0200

    Add numpy dep

--- setup.py
+++ setup.py
@@ -130,6 +130,7 @@ setup(
     description="Python bindings for the FRI Client SDK library.",
     long_description="",
     ext_modules=[CMakeExtension("pyFRIClient")],
+    install_requires=["numpy"],
     cmdclass={"build_ext": CMakeBuild},
     python_requires=">=3.8",
 )
