dc5c9f6f2618230dd75fadf5e457ebf2149383c1
mhubii <m.huber_1994@hotmail.de>
2023-08-05 11:43:38 +0100

    fetch content suggestion

---
+++ pyFRI/src/fri_config.h
@@ -0,0 +1,10 @@
+#ifndef FRI_CONFIG_H
+#define FRI_CONFIG_H
+
+// Indicates the major version of FRI.
+#define FRI_VERSION_MAJOR 1
+
+// Indicates the minor version of FRI.
+#define FRI_VERSION_MINOR 15
+
+#endif
